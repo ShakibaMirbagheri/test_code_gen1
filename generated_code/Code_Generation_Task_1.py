577f52126f9650d3f669bdcf620d825b2c37fbd4
Shakiba Mirbagheri <shakiba.mirbagheri@ict-group.it>
2025-08-06 15:02:42 +0200

    Add generated python file: Code_Generation_Task_1.py

---
+++ generated_code/Code_Generation_Task_1.py
@@ -0,0 +1,13 @@
+import uuid
+from typing import Any, Callable, Dict, List, Optional
+class Person(dict):
+    def __init__(self, name: str = None, surname: str = None) -> None:
+        self.__name__ = name
+        self.__surname__ = surname
+    @classmethod
+    def from_string(cls, string: str) -> "Person":
+        return cls(**{
+            'uuid': uuid.uuid4(),
+            'name': str(uuid.uuid4()),
+            'surname': str(surname),
+        })
