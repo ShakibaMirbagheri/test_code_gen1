3ad7ba812403acf6bbf6761b23ce17bc7e52f43d
Shakiba Mirbagheri <shakiba.mirbagheri@ict-group.it>
2025-08-06 17:29:15 +0200

    Add src/main/java/it/ness/testcodegen1/management/AppConstants.java

---
+++ src/main/java/it/ness/testcodegen1/management/AppConstants.java
@@ -0,0 +1,10 @@
+package it.ness.testcodegen1.management;
+
+
+public class AppConstants {
+
+public static final String API_PATH = "/api";
+public static final String GREETING_PATH = API_PATH + "/greetings";
+
+
+}
