9ceb6b8d8bb37f225992d0135f9411c84d4e8548
Shakiba Mirbagheri <shakiba.mirbagheri@ict-group.it>
2025-08-06 17:20:58 +0200

    Add src/main/java/it/ness/api/service/ValidationGroups.java

---
+++ src/main/java/it/ness/api/service/ValidationGroups.java
@@ -0,0 +1,14 @@
+package it.ness.api.validation;
+
+import jakarta.validation.groups.Default;
+
+public interface ValidationGroups {
+    interface PERSIST extends Default {
+    }
+
+    interface UPDATE extends Default {
+    }
+
+    interface DELETE extends Default {
+    }
+}
